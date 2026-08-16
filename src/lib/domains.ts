f088c90de11d83a3181d277a31ec1edbfa9af786
Nader youb <youb.nader@gmail.com>
2026-08-16 02:12:57 +0100

    feat: update domain levels with external links for resources

--- src/lib/domains.ts
+++ src/lib/domains.ts
@@ -20,9 +20,17 @@ export const DOMAINS: Domain[] = [
     title: "Licence Métiers de l'Informatique",
     accent: "info",
     levels: [
-      { code: "LMI1", label: "LMI1", href: "#" },
-      { code: "LMI2", label: "LMI2", href: "#" },
-      { code: "LMI3", label: "LMI3", href: "#" },
+      {
+        code: "LMI1",
+        label: "LMI1",
+        href: "https://insagbs-my.sharepoint.com/:f:/g/personal/nexusclub_insag_edu_dz/Ej7z3cFUlQxHsHMFcTfPDC8BQKQhfbrKatwOnjlqH8W2ng?e=wozqx4",
+      },
+      {
+        code: "LMI2",
+        label: "LMI2",
+        href: "https://insagbs-my.sharepoint.com/:f:/g/personal/nexusclub_insag_edu_dz/Em4TQW_J6y5HuR_fS1wnffoBFz_5Q_XHRY24Y54Zk7oSQw?e=lxsb1t",
+      },
+      // { code: "LMI3", label: "LMI3", href: "#" },
     ],
   },
   {
@@ -30,9 +38,21 @@ export const DOMAINS: Domain[] = [
     title: "Licence Marketing",
     accent: "marketing",
     levels: [
-      { code: "LMK1", label: "LMK1", href: "#" },
-      { code: "LMK2", label: "LMK2", href: "#" },
-      { code: "LMK3", label: "LMK3", href: "#" },
+      {
+        code: "LMK1",
+        label: "LMK1",
+        href: "https://insagbs-my.sharepoint.com/:f:/g/personal/nexusclub_insag_edu_dz/EhuySCHiiqNLtkuBxGswF7sBUQG9-jiQVJBJoM4KZWGg6Q?e=tR26VO",
+      },
+      {
+        code: "LMK2",
+        label: "LMK2",
+        href: "https://insagbs-my.sharepoint.com/:f:/g/personal/nexusclub_insag_edu_dz/ElE6px4MYJdCkUrtIMCtXEgB8OZRWVbaDrRtMHIC7KkvhQ?e=Vvg9GQ",
+      },
+      {
+        code: "LMK3",
+        label: "LMK3",
+        href: "https://insagbs-my.sharepoint.com/:f:/g/personal/nexusclub_insag_edu_dz/EjHDE50qHRZKmuyOq27eSAcBpTZ0E1vFdnAqiyZvwRcTRQ?e=FVs5u7",
+      },
     ],
   },
   {
@@ -40,15 +60,25 @@ export const DOMAINS: Domain[] = [
     title: "Licence Comptabilité et Finance",
     accent: "finance",
     levels: [
-      { code: "LCF1", label: "LCF1", href: "#" },
-      { code: "LCF2", label: "LCF2", href: "#" },
+      {
+        code: "LCF1",
+        label: "LCF1",
+        href: "https://insagbs-my.sharepoint.com/:f:/g/personal/nexusclub_insag_edu_dz/EimXHyOky1tOkihLF4dl89QBijqrkmdw1MWrdqdIrahiKA?e=td5Udh",
+      },
+      // { code: "LCF2", label: "LCF2", href: "#" },
     ],
   },
   {
     key: "extra",
     title: "Extra et Divers",
     accent: "extra",
-    levels: [{ code: "livre", label: "Livre", href: "#" }],
+    levels: [
+      {
+        code: "livre",
+        label: "Livre",
+        href: "https://insagbs-my.sharepoint.com/:f:/g/personal/nexusclub_insag_edu_dz/El9M2AgtmydEqlZhbjLNqLcBO4z7INvPJ_EJpTNH1u19fA?e=HZ0UoE",
+      },
+    ],
   },
 ];
 
